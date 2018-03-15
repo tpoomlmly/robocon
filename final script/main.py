3f927ba5939c8700ec634db211d2cc849026a8e8
tpoomlmly <Tommy@poolle.co.uk>
2018-03-15 18:12:13 +0000

    Sorted the markers a bit

--- final script/main.py
+++ final script/main.py
@@ -1,9 +1,11 @@
 from sr.robot import *
-import find_coords
+from math import sin, cos, radians, pi
+import time
 import gotocoords
 R = Robot.setup()
 R.init()
-R.servos[0] = 0 #This value will need to be edited
+for i in range(4):
+    R.servos[i] = 0 #stop all servos from rotating
 R.wait_start()
 
 def main():
@@ -26,6 +28,74 @@ def main():
         #go to it
         gotocoords.drive(dist=closest.dist - 0.2) #0.2 for clearance between camera and claw
         #pick it up
+
+
+#Start of coordinate finding code
+##class StaticMarker:
+##    def __init__(self, uid, coordinates=(0, 0, 0)):
+##        self.uid = uid
+##        self.coordinates = coordinates
+##        self.x = coordinates[0]
+##        self.y = coordinates[1]
+##        self.z = coordinates[2]
+
+#Define all the markers - (uid: (x, y, z))
+staticMarkers = {0: (0.5, 0, 0.3), 1: (1.5, 0, 0.3), 2: (2.5, 0, 0.3),
+                 3: (3.5, 0, 0.3), 4: (4.5, 0, 0.3), 5: (5.5, 0, 0.3),
+                 6: (6, 0.5, 0.3), 7: (6, 1.5, 0.3), 8: (6, 2.5, 0.3),
+                 9: (6, 3.5, 0.3), 10: (6, 4.5, 0.3), 11: (6, 5.5, 0.3),
+                 12: (5.5, 6, 0.3), 13: (4.5, 6, 0.3), 14: (3.5, 6, 0.3),
+                 15: (2.5, 6, 0.3), 16: (1.5, 6, 0.3), 17: (0.5, 6, 0.3),
+                 18: (0, 5.5, 0.3), 19: (0, 4.5, 0.3), 20: (0, 3.5, 0.3),
+                 21: (0, 2.5, 0.3), 22: (0, 1.5, 0.3), 23: (0, 0.5, 0.3)}
+
+def get(marker_list):
+    for marker in marker_list:
+        if(marker.info.code in staticMarkers):
+            #xdist = marker.dist * sin(radians(abs(marker.rot_y) + marker.orientation.rot_y))
+            #ydist = marker.dist * cos(radians(abs(marker.rot_y) + marker.orientation.rot_y))
+            dist_from_wall = marker.dist * cos(radians(marker.orientation.rot_y - marker.rot_y))
+            dist_along_wall = 0 - (marker.dist * sin(radians(marker.orientation.rot_y - marker.rot_y)))
+            
+            return xdist, ydist
+
+##Usage:
+##markers = R.see()
+##xdist, ydist = get(markers)
+##print "x: " + str(xdist)
+##print "y: " + str(ydist)
+#end of coordinate finding code
+
+#start of driving code
+#need to add steering
+import time
+from math import pi
+
+circumference = 0.26 * math.pi #c=pi*d
+rot_time = circumference / 0.64 #circumference / speed of wheels = circumferences per sec
+rot_speed = 360 / rot_time #
+
+def drive(dist=0, _dir=0, _time=0): #dir in degrees
+    if dist != 0:
+        #steering section - needs finishing
+        R.motors[0].m0.power =
+        time.sleep(_dir / rot_speed)
+        #end steering section
+        R.motors[0].m0.power = 42
+        R.motors[0].m1.power = 42
+        time.sleep(dist / 0.64) #time = distance / speed
+        R.motors[0].m0.power = 0
+        R.motors[0].m1.power = 0
+    elif time != 0:
+        R.motors[0].m0.power = 42
+        R.motors[0].m1.power = 42
+        time.sleep(_time)
+        R.motors[0].m0.power = 0
+        R.motors[0].m1.power = 0
+
+#Usage:
+#drive(dist=markers[0].dist)
+#end of driving code
 
 if __name__ == "__main__":
     while True:
