48c2121497e51d8ccd7104afdec477c60d954bc5
tpoomlmly <Tommy@poolle.co.uk>
2018-03-22 17:30:07 +0000

    Update motors test.py

--- test scripts/motors test.py
+++ test scripts/motors test.py
@@ -4,7 +4,7 @@ import time
 R = Robot()
 R.gpio.pin_mode(1, INPUT_ANALOG)
 while True:
-    reading = R.gpio.analog_read(1) * (25 / 4096) #Scale the signal to a value out of 100
+    reading = R.gpio.analog_read(1) * (25.0 / 4096.0) #Scale the signal to a value out of 100
     R.motors[0].m0.power = reading
     R.motors[0].m1.power = reading
     time.sleep(0.05)
