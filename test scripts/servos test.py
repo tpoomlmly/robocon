b1395079ea8e66a56ebf1079400cc1a2d6f5e456
tpoomlmly <Tommy@poolle.co.uk>
2018-03-30 16:22:13 +0100

    Fixed for testing

--- test scripts/servos test.py
+++ test scripts/servos test.py
@@ -1,15 +1,16 @@
 import time
 from sr.robot import *
 
-R = Robot.setup()
-R.init()
-R.servos[0] = 0
-R.wait_start()
+R = Robot()
+#R = Robot.setup()
+#R.init()
+#R.servos[0] = 0
+#R.wait_start()
 
 R.gpio.pin_mode(1, INPUT_ANALOG)
 
 while True:
-    reading = R.gpio.analog_read(1) *(100.0/8192.0) - 100
+    reading = int(R.gpio.analog_read(1) *(100.0/8192.0) - 100)
     print reading
-    R.servos[0] = reading
+    R.servos[1] = reading
     time.sleep(0.1)
