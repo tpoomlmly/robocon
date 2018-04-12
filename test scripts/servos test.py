208a7d9f318d3c5daaf0b962bcf9dd3af75cba41
tpoomlmly <Tommy@poolle.co.uk>
2018-04-12 16:40:55 +0100

    Modified to remove transistors and potentiometer

--- test scripts/servos test.py
+++ test scripts/servos test.py
@@ -2,22 +2,19 @@ import time
 import smbus
 from sr.robot import *
 
-#INITILIZATION
+#INITILISATION
 R = Robot.setup()
-for i in range(3):
-    R.gpio.pin_mode(i + 2, OUTPUT)# Transitor innitilization
+#R.gpio.pin_mode(1, OUTPUT)# Transitor innitilization
 R.init(smbus.SMBus(1))
 
-#
+#R.gpio.digital_write(1, True)# Power is allowed to flow into the servos
 for i in range(3):
-    R.gpio.digital_write(i+2, True)# Power is allowed to flow into the servos
     R.servos[i] = 0
 R.wait_start()
 
-R.gpio.pin_mode(1, INPUT_ANALOG)
-
 while True:
-    reading = int(R.gpio.analog_read(1) *(100.0/8192.0) - 100)
-    print reading
-    R.servos[1] = reading
-    time.sleep(0.1)
+    #reading = int(R.gpio.analog_read(1) *(100.0/8192.0) - 100)
+    R.servos[2] = 100
+    time.sleep(1)
+    R.servos[2] = -100
+    time.sleep(1)
