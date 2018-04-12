d296a90574694b15a915eb33b54e9e50a54c94d0
tpoomlmly <Tommy@poolle.co.uk>
2018-04-12 12:40:01 +0100

    Fixed the turning direction

--- test scripts/drive to cube.py
+++ test scripts/drive to cube.py
@@ -1,15 +1,12 @@
 from sr.robot import *
+import math
 import time
 R = Robot()
 
-circumference = 0.26 * math.pi #c=pi*d
-rot_time = circumference / 0.91 #circumference / speed of wheels = circumferences per sec
-rot_speed = 360 / rot_time #degrees per second
-
 def drive(dist=0, angle=0, _time=0): #dir in degrees
     if angle != 0:
-        R.motors[0].m0.power = (120 * int(angle > 0)) - 60
-        R.motors[0].m1.power = (120 * int(angle < 0)) - 60
+        R.motors[0].m0.power = (120 * int(angle < 0)) - 60
+        R.motors[0].m1.power = (120 * int(angle > 0)) - 60
         time.sleep(abs(angle) / rot_speed)
         R.motors[0].m0.power = 0
         R.motors[0].m1.power = 0
@@ -27,6 +24,10 @@ def drive(dist=0, angle=0, _time=0): #dir in degrees
         R.motors[0].m1.power = 0
 
 while(True):
-    markers = R.see()
+    circumference = ((analogRead(4) * (20.0/16384.0)) + 20) * math.pi #c=pi*d
+    rot_time = circumference / 0.91 #circumference / speed of wheels = circumferences per sec
+    rot_speed = 360 / rot_time #degrees per second
+    
+    markers = R.see(res=(1296, 736))
     if len(markers) > 0:
         drive(dist=markers[0].dist, angle=markers[0].rot_y)
