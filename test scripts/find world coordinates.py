14defad76a302f51d1ccca4f8eb5d36eafdfd3dd
tpoomlmly <Tommy@poolle.co.uk>
2018-04-13 18:02:36 +0100

    Added bearing finding code

--- test scripts/find world coordinates.py
+++ test scripts/find world coordinates.py
@@ -18,13 +18,13 @@ def get(marker_list):
             #ydist = marker.dist * cos(radians(abs(marker.rot_y) + marker.orientation.rot_y))
             dist_from_wall = marker.dist * cos(radians(marker.orientation.rot_y - marker.rot_y))
             dist_along_wall = 0 - (marker.dist * sin(radians(marker.orientation.rot_y - marker.rot_y)))
-            if (marker.info.code >= 0) and (marker.info.code <= 5):
+            if(5 >= marker.info.code >= 0):
                 ydist = dist_from_wall
                 xdist = dist_along_wall + staticMarkers[marker.info.code][0]
-            elif (marker.info.code >= 6) and (marker.info.code <= 11):
+            elif(11 >= marker.info.code >= 6):
                 ydist = dist_along_wall + staticMarkers[marker.info.code][1]
                 xdist = staticMarkers[marker.info.code][0] - dist_from_wall
-            elif (marker.info.code >= 12) and (marker.info.code <= 17):
+            elif(17 >= marker.info.code >= 12):
                 xdist = staticMarkers[marker.info.code][0] - dist_along_wall
                 ydist = staticMarkers[marker.info.code][1] - dist_from_wall
             else:
@@ -32,6 +32,11 @@ def get(marker_list):
                 xdist = dist_from_wall
             return xdist, ydist
 
+def getBearing(marker_list):
+    for marker in marker_list:
+        if marker.info.marker_type == MARKER_TOKEN:
+            return (int(17 >= marker.info.code >= 12) * 0 + int(11 >= marker.info.code >= 6) * 90 + int(5 >= marker.info.code >= 0) * 180 + 270) - marker.orientation.rot_y
+
 if __name__ == "__main__":
     while(True):
-        get(R.see())
+        print get(R.see(res=(1296, 736)))
