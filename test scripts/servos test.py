675003d16ea2fde330eded4a42667ede4dd10568
tpoomlmly <Tommy@poolle.co.uk>
2018-04-13 18:02:54 +0100

    Modified for claw

--- test scripts/servos test.py
+++ test scripts/servos test.py
@@ -2,9 +2,7 @@ import time
 import smbus
 from sr.robot import *
 
-#INITILISATION
 R = Robot.setup()
-#R.gpio.pin_mode(1, OUTPUT)# Transitor innitilization
 R.init(smbus.SMBus(1))
 
 #R.gpio.digital_write(1, True)# Power is allowed to flow into the servos
@@ -14,7 +12,9 @@ R.wait_start()
 
 while True:
     #reading = int(R.gpio.analog_read(1) *(100.0/8192.0) - 100)
-    R.servos[2] = 100
+    R.servos[1] = 30
+    print "open"
     time.sleep(1)
-    R.servos[2] = -100
+    R.servos[1] = -80
+    print "close"
     time.sleep(1)
